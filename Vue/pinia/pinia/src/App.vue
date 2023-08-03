9ac64375582d39395032c4c463bbc9f2e023a8b0
theskyworld <2456450687@qq.com>
2023-08-03 22:37:11 +0800

    coursePaths\Vue\pinia\pinia\src\usage * add basicUsage * fix App.vue * fix main.ts

---
+++ Vue/pinia/pinia/src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import Home from "./usage/basicUsage/components/Home.vue";
+import MapWritaleState from "./usage/basicUsage/components/MapWritaleState.vue";
+import MapState from "./usage/basicUsage/components/MapState.vue";
+import MapActions from "./usage/basicUsage/components/MapActions.vue";
+</script>
+
+<template>
+  <div>
+    <Home></Home>
+    <!-- <MapWritaleState></MapWritaleState> -->
+    <!-- <MapState></MapState> -->
+    <!-- <MapActions></MapActions> -->
+  </div>
+</template>
+
+<style scoped></style>
--- Vue/pinia/pinia/src/main.ts
+++ Vue/pinia/pinia/src/main.ts
@@ -1,5 +1,21 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import { createPinia } from "pinia";
+import {
+  addNewPropertyToStore,
+  logPluginParams,
+  addNewState,
+  addNewOptionToStore,
+  wrapActions,
+} from "./usage/basicUsage/store/plugins";
 
-createApp(App).mount('#app')
+const pinia = createPinia();
+pinia
+  .use(addNewPropertyToStore)
+  .use(logPluginParams)
+  .use(addNewState)
+  .use(addNewOptionToStore)
+  .use(wrapActions);
+// 相较于vuex4中注册的为全局唯一的store对象（从store/index.ts中导入），这里注册的为pinia
+createApp(App).use(pinia).mount("#app");
